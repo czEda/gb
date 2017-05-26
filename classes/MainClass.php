9351d3fe446b173b217d837734aa20c53164a888
czEda <czeda12@gmail.com>
2017-05-26 11:01:34 +0200

    ...

--- classes/MainClass.php
+++ classes/MainClass.php
@@ -3,7 +3,15 @@
 class MainClass {
 
     public function mainMethod() {
-        echo $this->Render();
+
+        $WebSocketApi = new WebSocketApi();
+        if ($WebSocketApi->sendSocket("Test", ["name" => "Patrik", "message" => "testing"]) === true) {
+            echo("Povedlo se! Nodejs vrací true!");
+        } else {
+            echo("Něco se nepovedlo! Node.js nevrací true!");
+        }
+
+        echo $this->render();
     }
 
     protected function render() {
